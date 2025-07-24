87d5dfb67f9be412ffa8ae3ae07fc07a6950163f
tkdgheee <sangho135790@gmail.com>
2025-07-24 17:22:44 +0900

    feat: 공통 컴포넌트(Button) 구현(#8)
    - 각 스타일을 명시하고 상수파일로 분리
    - 원하는 스타일의 버튼을 props로 받아서 사용할 수 있도록 구현

---
+++ src/components/common/button.tsx
@@ -0,0 +1,35 @@
+import type { ButtonVariant } from '@/constants/button'
+import { BUTTON_VARIANTS, DEFAULT_BUTTON_VARIANT } from '@/constants/button'
+import { cn } from '@/utils/cn'
+
+type ButtonProps = {
+  children: React.ReactNode
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
+  disabled?: boolean
+  variant?: ButtonVariant
+  className?: string
+}
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  onClick,
+  disabled = false,
+  variant = DEFAULT_BUTTON_VARIANT,
+  className,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={cn(
+        BUTTON_VARIANTS[variant],
+        { 'cursor-not-allowed': disabled },
+        className
+      )}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
