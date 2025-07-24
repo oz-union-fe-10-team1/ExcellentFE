08431b0a231fc295d8357a2209201b4899a3d1e6
han-nun <spshspshek@gmail.com>
2025-07-24 21:03:43 +0900

    feat: 공통 컴포넌트(Header)구현 (#6)
    - map 사용하여 header 구현
    - layout 파일 만들어 Outlet 추가

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,124 @@
+import Logo from '@/assets/icons/header/logo.svg'
+import Mypage from '@/assets/icons/header/mypage.svg'
+import Cart from '@/assets/icons/header/cart.svg'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Header = () => {
+  const [isLogin, setIsLogin] = useState(false)
+  const navigate = useNavigate()
+
+  const NAV_ITEMS = [
+    { label: '패키지', path: '/package' },
+    { label: '테스트', path: '/test' },
+    { label: '제품 검색', path: '/search' },
+    { label: '후기', path: '/feedback' },
+  ]
+
+  const USER_ICONS = [
+    { label: '마이페이지', src: Mypage, path: '/mypage' },
+    { label: '장바구니', src: Cart, path: '/cart' },
+  ]
+
+  const MY_PAGE_SUBMENU = [
+    { label: '내 정보', path: '/mypage/profile' },
+    { label: '로그아웃', path: '/logout' },
+  ]
+
+  /* 로그인 UI 체크 용 */
+  const handleLogin = () => {
+    setIsLogin(true)
+  }
+
+  return (
+    <header className="flex h-20 w-full items-center justify-around border-b border-[#d9d9d9]">
+      <div className="flex w-full items-center justify-around">
+        {/* 로고 */}
+        <img
+          src={Logo}
+          alt="메인 로고"
+          className="h-11 cursor-pointer"
+          onClick={() => navigate('/')}
+        />
+
+        {/* 메뉴 */}
+        <div className="flex items-center gap-15">
+          <nav>
+            <ul className="flex gap-15">
+              {NAV_ITEMS.map((item) => (
+                <li
+                  key={item.path}
+                  className="cursor-pointer text-lg font-semibold active:text-[#123123]"
+                  onClick={() => navigate(item.path)}
+                >
+                  {item.label}
+                </li>
+              ))}
+            </ul>
+          </nav>
+
+          {/* 로그인 / 유저 아이콘 (로그인 전)*/}
+          <div className="flex items-center">
+            {!isLogin ? (
+              <button
+                className="h-13 w-41 cursor-pointer rounded-[60px] bg-[#f2544b] font-semibold text-white"
+                onClick={handleLogin}
+              >
+                로그인 / 회원가입
+              </button>
+            ) : (
+              <>
+                {/* 마이페이지, 장바구니 (로그인 이후) */}
+                <div className="flex gap-8">
+                  {USER_ICONS.map((icon) =>
+                    icon.label === '마이페이지' ? (
+                      <div key={icon.label} className="group relative">
+                        <button
+                          aria-label={icon.label}
+                          onClick={() => navigate(icon.path)}
+                        >
+                          <img
+                            src={icon.src}
+                            alt={icon.label}
+                            className="h-8 w-5 cursor-pointer"
+                          />
+                        </button>
+
+                        {/* 마이페이지 서브메뉴 hover 메뉴*/}
+                        <ul className="absolute left-1/2 z-10 hidden w-32 -translate-x-1/2 bg-white text-center shadow-md group-hover:block">
+                          {MY_PAGE_SUBMENU.map((sub) => (
+                            <li
+                              key={sub.path}
+                              className="cursor-pointer px-4 py-2 text-sm hover:bg-gray-100"
+                              onClick={() => navigate(sub.path)}
+                            >
+                              {sub.label}
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    ) : (
+                      <button
+                        key={icon.label}
+                        aria-label={icon.label}
+                        onClick={() => navigate(icon.path)}
+                      >
+                        <img
+                          src={icon.src}
+                          alt={icon.label}
+                          className="h-8 w-5 cursor-pointer"
+                        />
+                      </button>
+                    )
+                  )}
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
