ddc6a677201e8c70c5cadf0f9b20dce030a03593
han-nun <spshspshek@gmail.com>
2025-07-24 21:01:02 +0900

    feat: tanstackquery 설정, routes 설정 (#6)
    - tanstackquery Provider 설정
    - BrouserRouter 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Layout from './pages/Layout'
+import HomePage from './pages'
+
+const queryClient = new QueryClient()
 
 function App() {
-  return <div>test</div>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            {/* 각자 페이지 만들어지면 등록 */}
+            <Route path="/" element={<HomePage />} />
+            <Route path="package" element={<HomePage />} />
+            <Route path="test" element={<HomePage />} />
+            <Route path="search" element={<HomePage />} />
+            <Route path="feedback" element={<HomePage />} />
+            <Route path="mypage/profile" element={<HomePage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  )
 }
 
 export default App
