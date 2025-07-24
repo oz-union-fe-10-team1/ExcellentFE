2979ef2d0f17bd83ad9ff85831dc9e3fd1f4ea52
soyeon1351 <pack6411351@gmail.com>
2025-07-24 16:59:03 +0900

    feat: FormInput 요소 type 지정(#10)

---
+++ src/components/common/Input/FormInput.types.ts
@@ -0,0 +1,16 @@
+export interface FormInputProps {
+  id?: string
+  label?: string
+  type?: 'text' | 'email' | 'password' | 'number'
+  value?: string
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  placeholder?: string
+  hasError?: boolean
+  errorMessage?: string
+  hasSuccess?: boolean
+  successMessage?: string
+  disabled?: boolean
+  onFocus?: (e: React.FocusEvent<HTMLInputElement>) => void
+  onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void
+  className?: string
+}
