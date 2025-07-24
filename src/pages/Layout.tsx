418154a15f3afa4d74ac8c9d99d3c8bc3843ea0a
han-nun <spshspshek@gmail.com>
2025-07-24 21:03:43 +0900

    feat: 공통 컴포넌트(Header)구현 (#6)
    - map 사용하여 header 구현
    - layout 파일 만들어 Outlet 추가

---
+++ src/pages/Layout.tsx
@@ -0,0 +1,15 @@
+import Header from '@/components/common/Header'
+import { Outlet } from 'react-router-dom'
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  )
+}
+
+export default Layout
