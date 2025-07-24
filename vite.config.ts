c21d04ed2677ab956b7c3ea0bc06c9bda47572cd
han-nun <spshspshek@gmail.com>
2025-07-24 15:46:29 +0900

    chore: 절대 경로 설정 (#11)
    - vite.config 내부 resolve, alias 추가
    - tsconfig.app 파일 내부 path 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-});
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@/assets': path.resolve(__dirname, './src/assets'),
+      '@/components': path.resolve(__dirname, './src/components'),
+      '@/hooks': path.resolve(__dirname, './src/hooks'),
+      '@/lib': path.resolve(__dirname, './src/lib'),
+      '@/pages': path.resolve(__dirname, './src/pages'),
+      '@/stores': path.resolve(__dirname, './src/stores'),
+      '@/types': path.resolve(__dirname, './src/types'),
+      '@/context': path.resolve(__dirname, './src/context'),
+      '@/mock': path.resolve(__dirname, './src/mock'),
+    },
+  },
+})
