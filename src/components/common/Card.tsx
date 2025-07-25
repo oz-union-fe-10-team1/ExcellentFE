153948639af554d5032324c9d49a6e2a98baeb82
creamymood <wbwbwlgid@naver.com>
2025-07-25 13:34:15 +0900

    fix : 코드리뷰 파트 수정

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -4,7 +4,7 @@ const Card = ({ imgSrc, imgAlt, title, subtitle }: CardProps) => {
   return (
     <div className="flex flex-col">
       {imgSrc ? (
-        <div className="mb-[20px] flex h-[290px] w-[300px] items-center justify-center overflow-hidden rounded-[6px] border-[1px] border-[#D9D9D9]">
+        <div className="mb-5 flex h-[290px] w-[300px] items-center justify-center overflow-hidden rounded-[6px] border-[1px] border-[#D9D9D9]">
           <img
             src={imgSrc}
             alt={imgAlt}
@@ -12,18 +12,16 @@ const Card = ({ imgSrc, imgAlt, title, subtitle }: CardProps) => {
           />
         </div>
       ) : (
-        <div className="mb-[20px] h-48 w-full rounded-lg bg-gray-200" />
+        <div className="mb-5 h-48 w-full rounded-lg bg-gray-200" />
       )}
 
       {title && (
-        <h2 className="w-[300px] text-[18px] text-balance text-[#333333]">
+        <h2 className="w-[300px] text-lg text-balance text-[#333333]">
           {title}
         </h2>
       )}
       {subtitle && (
-        <p className="mt-[4px] w-[300px] text-[15px] text-[#666666]">
-          {subtitle}
-        </p>
+        <p className="mt-[4px] w-[300px] text-[#666666]">{subtitle}</p>
       )}
     </div>
   )
