c0ed607c88f415fe51aa6eb1e7a6df8ac0342654
soyeon1351 <pack6411351@gmail.com>
2025-07-25 15:37:55 +0900

    fix: variant를 사용한 분기 처리로 수정 (#10)
    - 차후 추가될 인풋 대비에 타입에 variant를 추가하여 확장성 넓힘

---
+++ src/constants/FormInput.ts
@@ -0,0 +1,26 @@
+const INPUT_VARIANTS = {
+  default: [
+    'h-[60px]',
+    'w-[560px]',
+    'rounded-[6px]',
+    'border p-5',
+    'text-lg',
+    'placeholder:text-[#666666]',
+    'focus:outline-none',
+  ].join(' '),
+
+  search: [
+    'text-lg',
+    'placeholder:text-[#666666]',
+    'focus:outline-none',
+    'h-[33px]',
+    'w-[360px]',
+    'border-b-2',
+    'border-b-[#000000]',
+    'pr-10',
+    'pb-3',
+    'pl-[10px]',
+  ].join(' '),
+}
+
+export default INPUT_VARIANTS
