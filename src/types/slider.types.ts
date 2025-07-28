8083137999b566cdaa08c76a9fa887d9da03b0ae
soyeon1351 <pack6411351@gmail.com>
2025-07-28 16:40:49 +0900

    fix: 슬라이더 타입 추가 (#30)
    - 변하는 값과 단위를 나타내기 위한 타입 추가

--- src/types/slider.types.ts
+++ src/types/slider.types.ts
@@ -1,8 +1,11 @@
-export interface SliderProps {
+export type SliderProps = {
   defaultValue?: number[]
   max?: number
   step?: number
   label: string
-  variant: 'sweetness' | 'acidity' | 'body'
+  variant: 'sweetness' | 'acidity' | 'body' | 'trust'
   className?: string
+  value: number[]
+  onValueChange: (value: number[]) => void
+  formatValue?: (value: number) => string
 }
