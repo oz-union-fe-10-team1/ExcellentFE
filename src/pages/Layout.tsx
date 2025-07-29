f353f225bb93e5945fc3129783dcfc7ac9999254
tkdgheee <sangho135790@gmail.com>
2025-07-29 16:01:46 +0900

    feat: 공통 컴포넌트(Footer) 구현(#49)
    - 페이지 끝 도달 시 자동 표시되는 플로팅 푸터 구현
    - 회사 정보, 연락처, 사업자 정보 표시
    - 반응형 레이아웃 지원 (높이 255px)
    - 스크롤 이벤트 기반 표시/숨김 기능

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,5 +1,6 @@
 import Header from '@/components/common/Header'
 import { Outlet } from 'react-router-dom'
+import Footer from '@/components/common/Footer'
 
 const Layout = () => {
   return (
@@ -8,6 +9,7 @@ const Layout = () => {
       <main>
         <Outlet />
       </main>
+      <Footer />
     </>
   )
 }
