b2641470bb4c6d91c8f367f5a0916372c65cdd9e
creamymood <wbwbwlgid@naver.com>
2025-07-31 16:09:21 +0900

    feat: 시음 테스트 페이지 제작 후, 라우터 등록 (#73)
    - 시음 페이지 구현
    - app.tsx에도 등록

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Login from '@/pages/Login'
+import TestMain from './pages/tasteTest/TestMain'
 import SearchPage from '@/pages/SearchPage'
 
 const queryClient = new QueryClient()
@@ -19,7 +20,7 @@ function App() {
             {/* 각자 페이지 만들어지면 등록 */}
             <Route path="/" element={<HomePage />} />
             <Route path="package" element={<HomePage />} />
-            <Route path="test" element={<HomePage />} />
+            <Route path="test" element={<TestMain />} />
             <Route path="search" element={<SearchPage />} />
             <Route path="feedback" element={<HomePage />} />
             <Route path="mypage" element={<HomePage />} />
