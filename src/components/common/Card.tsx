fc93dc00851e05e4334103c7ada105bcfa6e6e06
creamymood <wbwbwlgid@naver.com>
2025-07-31 11:22:46 +0900

    fix: 코드 리뷰 후 수정 (#61)
    - 불필요한 연산자 수정
    - imgAlt 옵셔널에서 필수로 변경

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -4,22 +4,12 @@ const Card = ({ imgSrc, imgAlt, title, subtitle, price }: CardProps) => {
   return (
     <div className="flex w-[300px] flex-col">
       <div className="mb-5 flex h-[290px] w-full items-center justify-center overflow-hidden rounded-[6px] border border-[#D9D9D9] bg-gray-200">
-        {imgSrc ? (
-          <img
-            src={imgSrc}
-            alt={imgAlt}
-            className="h-full w-full object-cover"
-          />
-        ) : null}
+        <img src={imgSrc} alt={imgAlt} className="h-full w-full object-cover" />
       </div>
 
-      {title && (
-        <h2 className="text-lg text-balance text-[#333333]">{title}</h2>
-      )}
-
-      {subtitle && <p className="mt-1 text-[#666666]">{subtitle}</p>}
-
-      {price && <p className="mt-1 text-[15px] text-[#333333]">{price}원</p>}
+      <h2 className="text-lg text-balance text-[#333333]">{title}</h2>
+      <p className="mt-1 text-[#666666]">{subtitle}</p>
+      <p className="mt-1 text-[15px] text-[#333333]">{price}원</p>
     </div>
   )
 }
