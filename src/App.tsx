45fd2720b8b35ad5eab6ca88467a44ce00b0633e
soyeon1351 <pack6411351@gmail.com>
2025-08-01 14:17:55 +0900

    fix: SearchPage 등록 (#67)
    - 검색 페이지 등록

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Login from '@/pages/Login'
+import SearchPage from '@/pages/SearchPage'
 
 const queryClient = new QueryClient()
 
@@ -19,7 +20,7 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="package" element={<HomePage />} />
             <Route path="test" element={<HomePage />} />
-            <Route path="search" element={<HomePage />} />
+            <Route path="search" element={<SearchPage />} />
             <Route path="feedback" element={<HomePage />} />
             <Route path="mypage" element={<HomePage />} />
             <Route path="cart" element={<HomePage />} />
