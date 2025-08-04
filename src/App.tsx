b46111e91f1c5949ae7b0552f73c4772add5587e
dltmfql456 <dltmfql456@naver.com>
2025-08-04 16:36:42 +0900

    feat: 마이페이지의 레이아웃과 페이지 및 라우팅 추가 (#84)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import Layout from '@/components/layout/Layout'
 import HomePage from '@/pages'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
 import Login from '@/pages/Login'
-import TestMain from '@/pages/tasteTest'
 import SearchPage from '@/pages/SearchPage'
+import TestMain from '@/pages/tasteTest'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import './App.css'
+import MyPageLayout from './components/layout/MyPageLayout'
+import AccountEdit from './pages/my-page/AccountEdit'
+import OrderHistory from './pages/my-page/OrderHistory'
+import TasteProfile from './pages/my-page/TasteProfile'
+import TastingHistory from './pages/my-page/TastingHistory'
+import NotFound from './pages/NotFound'
 
 const queryClient = new QueryClient()
 
@@ -18,13 +24,24 @@ function App() {
 
           <Route element={<Layout />}>
             {/* 각자 페이지 만들어지면 등록 */}
+            {/* 메인 페이지 라우팅 */}
             <Route path="/" element={<HomePage />} />
             <Route path="package" element={<HomePage />} />
             <Route path="test" element={<TestMain />} />
             <Route path="search" element={<SearchPage />} />
             <Route path="feedback" element={<HomePage />} />
-            <Route path="mypage" element={<HomePage />} />
             <Route path="cart" element={<HomePage />} />
+
+            {/* 마이페이지 라우팅 */}
+            <Route path="mypage" element={<MyPageLayout />}>
+              <Route index element={<Navigate to="taste-profile" replace />} />
+              <Route path="taste-profile" element={<TasteProfile />} />
+              <Route path="account-edit" element={<AccountEdit />} />
+              <Route path="order-history" element={<OrderHistory />} />
+              <Route path="tasting-history" element={<TastingHistory />} />
+            </Route>
+
+            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </BrowserRouter>
