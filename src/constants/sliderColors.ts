9bf569a3ad091b0fb22b58775b5ab50246e77ee3
soyeon1351 <pack6411351@gmail.com>
2025-08-05 00:29:04 +0900

    style: UI 수정 (#90)
    - 슬라이더 추가

--- src/constants/sliderColors.ts
+++ src/constants/sliderColors.ts
@@ -3,6 +3,9 @@ export const VARIANT_COLOR_MAP = {
   acidity: '#99B278',
   body: '#3C72B4',
   trust: '#000000',
+  carbonation: '#6C4A5D',
+  bitter: '#A17950',
+  zest: '#95AD75',
 } as const
 
 export type SliderVariant = keyof typeof VARIANT_COLOR_MAP
