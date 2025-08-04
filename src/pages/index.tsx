7107df5db76d2309e9801b5e2c6a21df8283ff1c
dltmfql456 <dltmfql456@naver.com>
2025-08-04 16:41:34 +0900

    feat: z-index 관리 파일 추가 후 기존 z-index 리팩토링 (#84)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
+import { useNavigate } from 'react-router-dom'
+
 const HomePage = () => {
+  const navigate = useNavigate()
   return (
     <div>
       <h1>Home Page</h1>
+      <button
+        onClick={() => {
+          navigate('/mypage')
+        }}
+      >
+        마이페이지로 이동
+      </button>
     </div>
   )
 }
