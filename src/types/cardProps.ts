fcaf9035003597467a4a3580012426b7d6de8585
creamymood <wbwbwlgid@naver.com>
2025-08-06 21:45:36 +0900

    feat: 카드 컴포넌트 분기 처리를 위해 타입 재설정 (#94)
    - 카드 타입 설정

--- src/types/cardProps.ts
+++ src/types/cardProps.ts
@@ -1,7 +1,26 @@
-export interface CardProps {
+export interface CardBaseProps {
   imgSrc: string
   imgAlt: string
-  title: string
-  subtitle: string
-  price: number
+  title?: string
+  subtitle?: string
+  price?: number
+}
+
+// 리뷰 카드 타입
+export interface ReviewCardProps {
+  imgSrc: string
+  imgAlt: string
+  starRating?: number
+  userId?: string
+  review?: string
+}
+
+// 테스트 페이지 추천 카드 타입
+export interface TestCardProps {
+  imgSrc: string
+  imgAlt: string
+  title?: string
+  subtitle?: string
+  firstLabel?: string
+  secondLabel?: string
 }
