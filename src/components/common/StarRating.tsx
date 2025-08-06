11775931b167ec170617060df31bb4da58d74ece
han-nun <spshspshek@gmail.com>
2025-08-06 14:43:23 +0900

    feat: starRating 구현 (#95)
    - 비즈니스 로직 분리
    - 0.5 단위 체크 가능
    - ReadOnly 기능 추가로 읽기 전용 변경 가능

---
+++ src/components/common/StarRating.tsx
@@ -0,0 +1,55 @@
+import Icon from './Icon'
+import StarIcon from '@/assets/icons/starRating/Star.svg?react'
+import { cn } from '@/utils/cn'
+import type { StarRatingProps } from '@/types/starRating'
+import useStarRating from '@/hooks/useStarRating'
+
+const StarRating = ({
+  totalStars = 5,
+  defaultRating = 0,
+  onChange,
+  readOnly = false,
+  size,
+  className,
+}: StarRatingProps) => {
+  const { handleClick, getFillWidth } = useStarRating({
+    defaultRating,
+    onChange,
+    readOnly,
+  })
+
+  return (
+    <div className={`flex items-center ${className}`}>
+      {Array.from({ length: totalStars }).map((_, index) => (
+        <div
+          key={index}
+          className={cn(
+            'relative text-3xl select-none',
+            readOnly ? 'cursor-default' : 'cursor-pointer'
+          )}
+          onClick={(e) => handleClick(e, index)}
+        >
+          <span className="text-[#f2f2f2]">
+            <Icon
+              icon={StarIcon}
+              size={size}
+              className={readOnly ? 'cursor-default' : 'cursor-pointer'}
+            />
+          </span>
+          <span
+            className="absolute top-0 left-0 overflow-hidden text-[#f2544b]"
+            style={{ width: `${getFillWidth(index) * 100}%` }}
+          >
+            <Icon
+              icon={StarIcon}
+              size={size}
+              className={readOnly ? 'cursor-default' : 'cursor-pointer'}
+            />
+          </span>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default StarRating
