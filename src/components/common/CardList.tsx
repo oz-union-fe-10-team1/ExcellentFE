f43d341eadd9115dd2be24aba670c1611f11384b
creamymood <wbwbwlgid@naver.com>
2025-08-06 21:48:44 +0900

    feat: 공통 카드 리스트 컴포넌트 수정 (#94)
    - 카드 리스트 컴포넌트 수정 (타입 분기)

--- src/components/common/CardList.tsx
+++ src/components/common/CardList.tsx
@@ -1,15 +1,20 @@
-import type { CardProps } from '@/types/cardProps'
-import Card from './Card'
+import type {
+  CardBaseProps,
+  ReviewCardProps,
+  TestCardProps,
+} from '@/types/cardProps'
+import Card from './Card/Card'
 
-interface CardListProps {
-  cards: CardProps[]
-}
+type CardListProps =
+  | { type: 'default'; cards: CardBaseProps[] }
+  | { type: 'review'; cards: ReviewCardProps[] }
+  | { type: 'test'; cards: TestCardProps[] }
 
-const CardList = ({ cards }: CardListProps) => {
+const CardList = ({ cards, type }: CardListProps) => {
   return (
     <div className="mx-auto grid max-w-[1281px] grid-cols-4 gap-[27px]">
-      {cards.map((item, i) => (
-        <Card key={i} {...item} />
+      {cards.map((card, i) => (
+        <Card key={i} type={type} data={card} />
       ))}
     </div>
   )
