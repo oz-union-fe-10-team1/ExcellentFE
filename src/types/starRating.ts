0d269fed4ec04a37be52f72b6e445442b5ae87a6
han-nun <spshspshek@gmail.com>
2025-08-06 14:43:23 +0900

    feat: starRating 구현 (#95)
    - 비즈니스 로직 분리
    - 0.5 단위 체크 가능
    - ReadOnly 기능 추가로 읽기 전용 변경 가능

---
+++ src/types/starRating.ts
@@ -0,0 +1,11 @@
+export interface UseStarRatingProps {
+  defaultRating?: number
+  onChange?: (value: number) => void
+  readOnly?: boolean
+}
+
+export interface StarRatingProps extends UseStarRatingProps {
+  totalStars?: number
+  size: number
+  className?: string
+}
