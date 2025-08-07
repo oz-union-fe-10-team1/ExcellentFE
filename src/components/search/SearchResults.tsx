bafa98eb7b4185b221c08bc43102b2c5099201e9
soyeon1351 <pack6411351@gmail.com>
2025-08-07 16:11:26 +0900

    refactor: 컴포넌트 분리 (#96)
    - SearchForm: 검색 입력 컴포넌트
    - FeatureFilter: 체크박스 필터 컴포넌트
    - SliderGroup: 슬라이더 그룹 컴포넌트
    - SearchResults: 검색 결과 컴포넌트

---
+++ src/components/search/SearchResults.tsx
@@ -0,0 +1,59 @@
+import CardList from '@/components/common/CardList'
+import Pagination from '@/components/common/Pagination'
+import type { SearchResultsProps } from '@/types/search'
+
+const SearchResults = ({
+  data,
+  isLoading,
+  isError,
+  currentPage,
+  totalPages,
+  onPageChange,
+}: SearchResultsProps) => {
+  const renderContent = () => {
+    if (isLoading) {
+      return <div className="text-center text-lg font-medium">로딩 중...</div>
+    }
+
+    if (isError) {
+      return (
+        <div className="text-center text-lg font-medium text-red-600">
+          에러가 발생했습니다.
+        </div>
+      )
+    }
+
+    return (
+      <div>
+        <CardList
+          cards={data.map((product) => ({
+            imgSrc: product.main_image_url,
+            imgAlt: product.name,
+            title: product.name,
+            subtitle: product.short_description,
+            price: product.price,
+          }))}
+        />
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={onPageChange}
+        />
+      </div>
+    )
+  }
+
+  return (
+    <div className="flex flex-col items-center">
+      <div>
+        <h2 className="mb-[19px] text-[24px] font-bold text-[#333333]">
+          검색 결과
+        </h2>
+        <hr className="mb-5 w-[1280px] border-2 border-[#000000]" />
+      </div>
+      <div className="mb-25 flex flex-col gap-20">{renderContent()}</div>
+    </div>
+  )
+}
+
+export default SearchResults
