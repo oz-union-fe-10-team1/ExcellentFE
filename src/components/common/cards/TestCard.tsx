9099aac8c9953037e7fd6e6d0faf33238ac70121
creamymood <wbwbwlgid@naver.com>
2025-08-10 13:18:43 +0900

    feat: TestCard ui 디자인 수정 (#103)
    - 테스트 카드 일부 수정

--- src/components/common/cards/TestCard.tsx
+++ src/components/common/cards/TestCard.tsx
@@ -13,9 +13,9 @@ const TestCard = ({
       <div className="relative mb-[9px] flex h-[125px] w-full items-center justify-center overflow-hidden rounded-[6px] border border-[#D9D9D9] bg-gray-200">
         <img src={imgSrc} alt={imgAlt} className="h-full w-full object-cover" />
       </div>
-      <p className="mb-[13px] text-sm font-bold text-[#333333]"> {title} </p>
-      <p className="mb-2.5 text-xs font-bold text-[#666666]">{subtitle}</p>
-      <div className="flex justify-between">
+      <p className="mb-[9px] text-sm font-bold text-[#333333]"> {title} </p>
+      <p className="mb-[13px] text-xs font-bold text-[#666666]">{subtitle}</p>
+      <div className="flex gap-1">
         <p className="flex h-[25px] items-center justify-center bg-[#FFFFFF] px-2 py-[5px] text-[11px] text-[#F2544B]">
           {firstLabel}
         </p>
