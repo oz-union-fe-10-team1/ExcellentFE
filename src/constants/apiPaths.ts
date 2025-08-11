b905d30ad3408826b964433ece8b8ef678068cff
soyeon1351 <pack6411351@gmail.com>
2025-08-11 16:25:56 +0900

    fix: 코드 수정 (#96)
    - api 사용 코드 수정

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,12 +1,15 @@
 import type { SocialProvider } from '@/types/auth'
 
 export const BASE_URL = import.meta.env.VITE_API_URL
-export const PRODUCTS_SEARCH_PATH = '/products/search'
 
 export const API_PATHS = {
   AUTH: {
     LOGIN: (provider: SocialProvider) => `/auth/login/${provider}`,
     REFRESH: '/auth/refresh',
     STATE: '/auth/state',
+  },
+
+  PRODUCTS: {
+    SEARCH: 'api/vi/products/search',
   },
 } as const
