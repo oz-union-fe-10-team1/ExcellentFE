a3fb779ea25a583255c9185c11113cd359ea4759
han-nun <spshspshek@gmail.com>
2025-08-11 16:13:55 +0900

    feat: 마이페이지 (시음 후기 모달) 구현 (#102)
    - 필요없는 파일 제거
    - 타입 추가

--- src/hooks/useSubmitFeedback.ts
+++ src/hooks/useSubmitFeedback.ts
@@ -1,12 +1,10 @@
-import { useMutation } from '@tanstack/react-query'
 import { feedbackApi } from '@/api/feedback'
 import type { FeedbackRequest } from '@/api/feedback/types'
+import { useMutation } from '@tanstack/react-query'
 
 const useSubmitFeedback = () => {
   return useMutation({
-    mutationFn: async (data: FeedbackRequest) => {
-      return feedbackApi.submit(data)
-    },
+    mutationFn: (data: FeedbackRequest) => feedbackApi.submit(data),
   })
 }
 
