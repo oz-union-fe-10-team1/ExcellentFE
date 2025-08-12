9948916ef52139f5f677a67d78155964b0316896
han-nun <spshspshek@gmail.com>
2025-08-12 13:45:01 +0900

    feat: 마이페이지 (시음 후기 모달) 구현 (#102)
    - 에러처리 추가
    - 태그 최소 갯수 추가

--- src/hooks/useSubmitFeedback.ts
+++ src/hooks/useSubmitFeedback.ts
@@ -5,6 +5,15 @@ import { useMutation } from '@tanstack/react-query'
 const useSubmitFeedback = () => {
   return useMutation({
     mutationFn: (data: FeedbackRequest) => feedbackApi.submit(data),
+
+    /* TODO: 추후 슬비님 PR 들어오면 수정 예정 임시 alert */
+    onSuccess: () => {
+      alert('후기 등록이 완료되었습니다.')
+    },
+
+    onError: () => {
+      alert('후기 등록이 실패했습니다.')
+    },
   })
 }
 
