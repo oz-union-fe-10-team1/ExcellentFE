245f53a85c71da04328c32435d51d7c75fa41065
creamymood <wbwbwlgid@naver.com>
2025-08-13 12:31:44 +0900

    feat: 문항 받아오기 경로 상수화 (#103)
    - 경로 상수화

--- src/api/tasteTest.ts
+++ src/api/tasteTest.ts
@@ -1,7 +1,13 @@
-import axios from 'axios'
+import { API_PATHS } from '@/constants/apiPaths'
+import type { TasteTestProfile } from '@/types/tasteTypes'
+import { axiosInstance } from '@/utils/axios'
 
+export const getTasteTestProfile = async (): Promise<TasteTestProfile> => {
+  const { data } = await axiosInstance.get(API_PATHS.TASTE_TEST.PROFILE)
+  return data
+}
 //테스트 문항 가져오기 함수
 export const getTestQuetion = async () => {
-  const res = await axios.get('/api/v1/taste-test/questions')
+  const res = await axiosInstance.get(API_PATHS.TASTE_TEST.GETTEST)
   return res.data
 }
