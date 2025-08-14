33e02cef5d25e97b27245f36f83874cc3077a2e8
han-nun <spshspshek@gmail.com>
2025-08-14 14:42:37 +0900

    feat: 마이페이지 Cart 아이템이 없을 때 분기 (#119)
    - item의 길이가 1개 이상일 때만 컨텐츠 나올 수 있게 처리

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -23,28 +23,32 @@ const Cart = () => {
     <div className="mt-25 flex flex-col items-center justify-center">
       <h1 className="text-[40px] font-bold text-[#333333]">장바구니</h1>
       <ItemRowContent type="cart" items={CART_ITEMS} />
-      <div className="mt-25 h-20 w-320 bg-[#f2f2f2]">
-        <div className="flex h-full w-full items-center justify-center gap-7">
-          <div className="text-[#333333]">
-            상품금액 합계
-            <span className="ml-3 text-2xl font-bold">
-              {parseInt(data?.total_price || '0').toLocaleString()}
-            </span>
+      {CART_ITEMS.length > 0 && (
+        <>
+          <div className="mt-25 h-20 w-320 bg-[#f2f2f2]">
+            <div className="flex h-full w-full items-center justify-center gap-7">
+              <div className="text-[#333333]">
+                상품금액 합계
+                <span className="ml-3 text-2xl font-bold">
+                  {parseInt(data?.total_price || '0').toLocaleString()}
+                </span>
+              </div>
+              <div className="flex h-7 w-7 items-center justify-center rounded-[50%] bg-[#f2544b] text-center text-lg font-bold text-[#ffffff]">
+                <Equal size={15} strokeWidth={4} />
+              </div>
+              <div className="text-[#333333]">
+                총 결제 금액
+                <span className="ml-3 text-2xl font-bold">
+                  {parseInt(data?.final_total || '0').toLocaleString()}
+                </span>
+              </div>
+            </div>
           </div>
-          <div className="flex h-7 w-7 items-center justify-center rounded-[50%] bg-[#f2544b] text-center text-lg font-bold text-[#ffffff]">
-            <Equal size={15} strokeWidth={4} />
-          </div>
-          <div className="text-[#333333]">
-            총 결제 금액
-            <span className="ml-3 text-2xl font-bold">
-              {parseInt(data?.final_total || '0').toLocaleString()}
-            </span>
-          </div>
-        </div>
-      </div>
-      <Button variant="VARIANT4" className="mt-12 mb-25">
-        결제하기
-      </Button>
+          <Button variant="VARIANT4" className="mt-12 mb-25">
+            결제하기
+          </Button>
+        </>
+      )}
     </div>
   )
 }
