2fedafdec9097150f83fe751d1ba579f73185039
han-nun <spshspshek@gmail.com>
2025-08-19 13:24:20 +0900

    style: cardlist 스타일 변경 (#162)
    - 강제되는 grid 제거
    - flex, flex-wrap 사용

--- src/components/common/cards/CardList.tsx
+++ src/components/common/cards/CardList.tsx
@@ -7,20 +7,15 @@ import type {
 import Card from '@/components/common/cards/Card.tsx'
 
 type CardListProps =
-  | { type: 'default'; cards: CardBaseProps[]; columns?: 3 | 4 }
-  | { type: 'review'; cards: ReviewCardProps[]; columns?: 3 | 4 }
-  | { type: 'test'; cards: TestCardProps[]; columns?: 3 | 4 }
-  | { type: 'best'; cards: BestReviewCardProps[]; columns?: 3 | 4 }
+  | { type: 'default'; cards: CardBaseProps[] }
+  | { type: 'review'; cards: ReviewCardProps[] }
+  | { type: 'test'; cards: TestCardProps[] }
+  | { type: 'best'; cards: BestReviewCardProps[] }
 
 const CardList = (props: CardListProps) => {
-  const columns = props.columns ?? 4
-
-  const colClass = columns === 3 ? 'grid-cols-3' : 'grid-cols-4'
-
   return (
-    <div className={`mx-auto grid max-w-[1281px] ${colClass} gap-[27px]`}>
+    <div className="flex max-w-[1281px] flex-wrap gap-[27px]">
       {props.cards.map((card, i) => {
-        // 판별 유니온에 따라 타입 단언
         switch (props.type) {
           case 'default':
             return <Card key={i} type="default" data={card as CardBaseProps} />
