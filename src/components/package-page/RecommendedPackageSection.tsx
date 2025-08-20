5030739344164f41a691efd7748e8503ce7f4043
han-nun <spshspshek@gmail.com>
2025-08-20 13:39:11 +0900

    feat: 패키지 페이지, 후기 모달 닉네임 추가 (#184)
    - 전역으로 관리하고 있는 프로필을 가져와 사용
    - 후기 모달에서 사용자 닉네임을 사용
    - 패키지 페이지에서 사용자 닉네임을 사용

--- src/components/package-page/RecommendedPackageSection.tsx
+++ src/components/package-page/RecommendedPackageSection.tsx
@@ -1,11 +1,13 @@
 import type { CardBaseProps } from '@/types/cardProps'
 import CardList from '@/components/common/cards/CardList'
+import { useAuthStore } from '@/stores/authStore'
 
 const RecommendedPackageSection = ({
   recommendedCards,
 }: {
   recommendedCards: CardBaseProps[]
 }) => {
+  const { user } = useAuthStore()
   return (
     <section className="py-16">
       <div className="container mx-auto">
@@ -13,7 +15,9 @@ const RecommendedPackageSection = ({
           <h2 className="mb-4 text-3xl font-bold text-[#333333]">
             추천 패키지
           </h2>
-          <p className="text-[#666666]">오직 {}의 취향을 반영한 패키지</p>
+          <p className="text-[#666666]">
+            오직 {user?.user_info?.nickname}만의 취향을 반영한 패키지
+          </p>
         </div>
         <CardList type="default" cards={recommendedCards} />
       </div>
