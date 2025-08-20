b0872f6a6ce52d4b1040f7488fd12a4649ed8bb4
soyeon1351 <pack6411351@gmail.com>
2025-08-20 11:33:10 +0900

    fix: msw 삭제 (#170)
    - 불필요한 파일 삭제

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,12 +1,10 @@
 import mypageHandlers from './handlers/mypage/mypage'
-// import productHandlers from './handlers/product/product'
 import recommendationHandlers from './handlers/recommendation/recommendation'
 import tastetestHandlers from './handlers/tastetest/tastetest'
 import userHandlers from './handlers/user/user'
 
 export const handlers = [
   ...mypageHandlers,
-  // ...productHandlers,
   ...recommendationHandlers,
   ...tastetestHandlers,
   ...userHandlers,
